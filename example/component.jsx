123c7d2378b418160e5e67e0bcdda20e822c9f15
Lucas Mosele <lucasmosele@gmail.com>
2016-07-15 15:31:53 -0400

    pass in size and animation props

--- example/component.jsx
+++ example/component.jsx
@@ -3,14 +3,8 @@ import Spinner from '../index.jsx';
 
 const App = React.createClass({
   render: function() {
-    const style = {
-      height: 50,
-      width: 50,
-    };
     return (
-      <div>
-        <Spinner style={style} pathColor="blue" />
-      </div>
+      <Spinner pathColor='gray' size='small' animation='slow' />
     );
   }
 });
--- index.jsx
+++ index.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 
 const Spinner = React.createClass({
-   
+   createClassName: function() {
+    let myClassName = 'react-svg-spinner';
+    myClassName = `${myClassName} ${"spinner-size-" + this.props.size}`;
+    myClassName =  `${myClassName} ${"spinner-animation-" + this.props.animation}`;
+    // myClassName = `${myClassName} ${this.props.blurEffect}`;
+    return myClassName;
+  },
   render: function() {
-    
+    const myFinalClassName = this.createClassName();
     return (
         <svg
-          pathColor={this.props.pathColor}
-         className={'react-svg-spinner ' + (this.props.className || '')} 
+         className={myFinalClassName} 
          viewBox="0 0 66 66">
           <circle className={'react-svg-path ' + (this.props.pathColor || '')} fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
         </svg>
     );
   },
-
   getDefaultProps: function() {
     return {
-      pathColor: 'gray'
+      pathColor: 'gray',
+      size: 'medium',
+      animation: 'medium'
     };
   }
 });
-
 export default Spinner;
